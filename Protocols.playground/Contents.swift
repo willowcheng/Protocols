dea7935ed1ec40deafda17c30c0f6e1508de8221
willowcheng <e.wingus@Gmail.com>
2015-07-03 12:30:27 -0400

    Finish up protocols

--- Protocols.playground/Contents.swift
+++ Protocols.playground/Contents.swift
@@ -66,3 +66,81 @@ class HourlyEmployee: Employee, Payable {
 class SalariedEmployee: Employee {
     let salary = 40000
 }
+
+// Modeling Loose Relationships
+
+protocol Blendable {
+    func blend()
+}
+
+class Fruit: Blendable {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func blend() {
+        println("I'm mush!")
+    }
+}
+
+class Dairy {
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+class Cheese: Dairy {}
+
+class Milk: Dairy, Blendable {
+    func blend() {
+        println("I haven't changed, I'm still milk!")
+    }
+}
+
+func makeSmoothie(ingredients: [Blendable]) {
+    println("Smoothie time!")
+}
+
+let strawberry = Fruit(name: "Strawberry")
+let cheddar = Cheese(name: "Cheddar Cheese")
+let chocolateMilk = Milk(name: "Chocolate")
+
+let ingredients: [Blendable] = [strawberry, chocolateMilk]
+makeSmoothie(ingredients)
+
+// Random Number Generator
+protocol RandomNumberGenerator {
+    func random() -> Double
+}
+
+class LinearCongruentialGenerator: RandomNumberGenerator {
+    var lastRandom = 42.0
+    let m = 139968.0
+    let a = 3877.0
+    let c = 29573.0
+    
+    func random() -> Double {
+        lastRandom = ((lastRandom * a + c) % m)
+        return lastRandom/m
+    }
+}
+
+class Dice {
+    let sides: Int
+    let generator: RandomNumberGenerator
+    
+    init(sides: Int, generator: RandomNumberGenerator) {
+        self.sides = sides
+        self.generator = generator
+    }
+    
+    func roll() -> Int {
+        return Int(generator.random() * Double(sides)) + 1
+    }
+}
+
+var d6 = Dice(sides: 6, generator: LinearCongruentialGenerator())
