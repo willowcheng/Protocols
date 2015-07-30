407ee1c8d39fdd9472f3c0239348344a3c846cba
willowcheng <e.wingus@Gmail.com>
2015-07-30 19:26:17 -0400

    Building flexible objects

--- AdvancedProtocols.playground/Contents.swift
+++ AdvancedProtocols.playground/Contents.swift
@@ -9,3 +9,29 @@ protocol Printable {
 protocol PrettyPrintable: Printable {
     func prettyDescription() -> String
 }
+
+// Vehicle Protocol
+
+protocol Brakeable {
+    var brakes: String { get }
+    func stop()
+    func slamOnBrakes()
+}
+
+protocol Drivable {
+    var steeringWheel: String { get }
+    var tires: String { get set }
+    func startEngine()
+    func stopEngine()
+    func turnLeft()
+    func turnRight()
+    func reverse()
+}
+
+protocol Vehicle: Brakeable, Drivable {
+    var numberOfSeats: Int { get }
+}
+
+protocol Car: Brakeable, Drivable {
+    var numberOfDoors: Int { get }
+}
